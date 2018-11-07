0e1435cd3e13e7e92acf022e2819d1224911b176
Alan K <akillian@outlook.com>
2018-11-07 14:17:44 -0500

    commit

--- send_mail.php
+++ send_mail.php
@@ -23,12 +23,11 @@ $transport = (new Swift_SmtpTransport('smtp.example.org', 25))
 $mailer = new Swift_Mailer($transport);
 
 // Create a message
-$message = (new Swift_Message($subject))
-  ->setFrom($name)
-  ->setTo($to)
-  ->setBody($body)
+$message = (new Swift_Message('Wonderful Subject'))
+  ->setFrom(['john@doe.com' => 'John Doe'])
+  ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
+  ->setBody('Here is the message itself')
   ;
-
 // Send the message
 $result = $mailer->send($message);
 
