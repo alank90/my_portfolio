487d8088649c289300b7bf1da0da4062e0540b15
Alan K <akillian@outlook.com>
2018-11-27 13:58:51 -0500

    commit

--- send_mail.php
+++ send_mail.php
@@ -22,11 +22,13 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // https://github.com/sendgrid/sendgrid-php/releases
 
 $email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("jdoe@aol.com", "Example User");
-$email->setSubject($subject);
-$email->addTo("akillian@outlook.com", "Example User");
-$email->addContent("text/plain",  "and easy to do anywhere, even with PHP");
-
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
